2b437349b333b31a7b386533a52eede432ed039d
Max Eddy <max@alley.co>
2018-08-08 14:19:47 -0700

    Make utils easier to import.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,4 @@
-const loaderUtils = require('loader-utils');
-
-/**
- *
- */
-module.exports = function loader() {};
-
-/**
- *
- * @param request
- * @returns {string}
- */
-module.exports.pitch = function prepareModule(request) {
-  if (this.cacheable) {
-    this.cacheable();
-  }
-
-  const path = loaderUtils.stringifyRequest(this, `!!${request}`);
-  return `
-    let content = require(${path});
-
-    if (typeof content === 'string') {
-      content = [[module.id, content, '']];
-    }
-
-    // Export CSS Modules
-    if (content.locals) {
-      module.exports = content.locals;
-    }
-
-    // Foo
-    module.exports.getCss = function() {
-      return content.toString();
-    };
-  `;
-};
+export { default as CriticalCssBuilder } from './CriticalCssBuilder';
+export { default as filterCriticalCss } from './filterCriticalCss';
+export { default as StyleContext } from './styleContext';
+export { default as withStyles } from './withStyles';
